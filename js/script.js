ce2d4fe244589f91b1fdefaa430d4f16afc85c82
2Diomedes <t.martinteyssier@gmail.com>
2018-06-12 11:09:17 +0200

    Array is done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
 var columnsS = 7;
 var rowsS = 6;
+
 var rows = [];
+var launch = [];
+
+var launcher = document.createElement('table');
+document.body.appendChild(launcher);
+
+for (var i = 0; i < rowsS-5; i++) {
+
+  var trL = document.createElement('tr');
+  launcher.appendChild(trL);
+
+  for (var j = 0; j < columnsS; j++) {
+
+    var columns = [j];
+    rows[i] = columns;
+    var thL = document.createElement('th');
+    trL.appendChild(thL);
+    // columns.push(j);
+
+  }
+
+}
+
 var table = document.createElement('table');
 document.body.appendChild(table);
-var tr = document.createElement('tr');
-var th = document.createElement('th');
+
 for (var i = 0; i < rowsS; i++) {
-    var slot = "";
-    rows.push(i);
+  var slot = "";
+  rows.push(i);
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+
   for (var j = 0; j < columnsS; j++) {
+
     var columns = [j];
-    slot += "( )";
     rows[i] = columns;
+    var th = document.createElement('th');
+    tr.appendChild(th);
     // columns.push(j);
+
+  }
+
 }
-console.log(slot);
-}
-console.log(rows);
+
+
 // console.log(columns);
