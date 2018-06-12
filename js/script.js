60d4ce86950685a3fc51bd742e0f65018c2e73b9
2Diomedes <t.martinteyssier@gmail.com>
2018-06-12 14:18:23 +0200

    We're working on functions

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
-var columnsS = 7;
 var rowsS = 6;
+var columnsS = 7;
 
 var rows = [];
+var rowsL = [];
 var launch = [];
 
 var launcher = document.createElement('table');
 document.body.appendChild(launcher);
 
-for (var i = 0; i < rowsS-5; i++) {
+for (var i = 0; i < rowsS - 5; i++) {
 
   var trL = document.createElement('tr');
   launcher.appendChild(trL);
 
   for (var j = 0; j < columnsS; j++) {
 
-    var columns = [j];
-    rows[i] = columns;
+    var columnsL = [j];
+    rowsL[i] = columnsL;
     var thL = document.createElement('th');
     trL.appendChild(thL);
-    // columns.push(j);
-
+    thL.innerText = j;
   }
-
 }
 
 var table = document.createElement('table');
@@ -39,19 +38,21 @@ for (var i = 0; i < rowsS; i++) {
     rows[i] = columns;
     var th = document.createElement('th');
     tr.appendChild(th);
-    // columns.push(j);
+  }
+}
 
+for (var i = 0; i < rowsL.length; i++) {
+  this.addEventListener("click", function() {
+      console.log('iypi');
+
+    })
   }
 
+function search(columns) {
+  for (var i = 0; i < 6; i++) {
+    console.log(columns + '>>' + i);
+    if (rows[i][columns] != 0) {
+      return i - 1;
+    }
+  }
 }
-for (var i = 0; i < launcher.length; i++) {
-
-    this.addEventListener("click", function(){
-        var tableRef = launcher[i];
-        for (var i = 0; i < table.length; i++) {
-            table[i]
-        }
-    })
-}
-
-// console.log(columns);
