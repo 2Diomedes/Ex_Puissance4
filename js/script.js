85cd9d0717d7decfd6082420bcaf2a7d40a34999
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-06-12 10:32:44 +0200

    begin to js and create a table, show an array into a body

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-var slot = "";
-var columns = 7;
-var rows = 6;
-
-for (var i = 0; i < rows; i++) {
-  for (var i = 0; i < columns; i++) {
+var columnsS = 7;
+var rowsS = 6;
+var rows = [];
+var table = document.createElement('table');
+document.body.appendChild(table);
+var tr = document.createElement('tr');
+var th = document.createElement('th');
+for (var i = 0; i < rowsS; i++) {
+    var slot = "";
+    rows.push(i);
+  for (var j = 0; j < columnsS; j++) {
+    var columns = [j];
     slot += "( )";
-    console.log(slot);
-  }
+    rows[i] = columns;
+    // columns.push(j);
 }
+console.log(slot);
+}
+console.log(rows);
+// console.log(columns);
