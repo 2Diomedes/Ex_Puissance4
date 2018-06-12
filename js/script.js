04610f8e50a95f6678a5b1895017a44f880f25c0
2Diomedes <t.martinteyssier@gmail.com>
2018-06-12 16:41:36 +0200

    AddEventListener works but doesn't select right cell yet

--- js/script.js
+++ js/script.js
@@ -12,47 +12,42 @@ for (var i = 0; i < rowsS - 5; i++) {
   var trL = document.createElement('tr');
   launcher.appendChild(trL);
 
+  var thL = [];
   for (var j = 0; j < columnsS; j++) {
 
-    var columnsL = [j];
-    rowsL[i] = columnsL;
-    var thL = document.createElement('th');
-    trL.appendChild(thL);
-    thL.innerText = j;
+    columnsL = [j];
+
+    thL[j] = document.createElement('th');
+    trL.appendChild(thL[j]);
+
+    select = thL[j];
+
+    thL[j].innerText = j;
+
+    select.addEventListener('click', function(){
+      console.log(this.innerText);
+      slot[1][5].style.background = 'red';
+    })
+
   }
 }
 
 var table = document.createElement('table');
 document.body.appendChild(table);
 
+var slot = [];
+
 for (var i = 0; i < rowsS; i++) {
-  var slot = "";
   rows.push(i);
-  var tr = document.createElement('tr');
-  table.appendChild(tr);
+  slot[i] = document.createElement('tr');
+  table.appendChild(slot[i]);
 
   for (var j = 0; j < columnsS; j++) {
 
     var columns = [j];
     rows[i] = columns;
-    var th = document.createElement('th');
-    tr.appendChild(th);
+
+    slot[i][j] = document.createElement('th');
+    slot[i].appendChild(slot[i][j]);
   }
 }
-
-// for (var i = 0; i < rows.length; i++) {
-//   cell = rows[i];
-//   cell.addEventListener('click', function(){
-//     console.log(cell);
-//   })
-// }
-
-
-// function search(columns) {
-//   for (var i = 0; i < 6; i++) {
-//     console.log(columns + '>>' + i);
-//     if (rows[i][columns] != 0) {
-//       return i - 1;
-//     }
-//   }
-// }
